7ba6d4c5ee0ba530dcd71de844b28cbf4aab860a
CybertekSchool <github@cybertekschool.com>
2018-08-14 19:55:38 -0400

    added step defs

---
+++ src/test/java/com/ctbnb/step_definitions/MyInfoStepDefs.java
@@ -0,0 +1,28 @@
+package com.ctbnb.step_definitions;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class MyInfoStepDefs {
+
+	@Given("user logs in using {string} {string}")
+	public void user_logs_in_using(String string, String string2) {
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
+
+	@When("the user is on the my self page")
+	public void the_user_is_on_the_my_self_page() {
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
+
+	@Then("user info should match the db records")
+	public void user_info_should_match_the_db_records() {
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
+
+}
